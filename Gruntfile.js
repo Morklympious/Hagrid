ea24e8f3a40570a17036e731d94435aeb89b16fa
Bradley Stafford <Bradleypstafford@gmail.com>
2015-11-26 15:44:01 -0800

    Doc site build process in place via assemble

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,55 @@
 module.exports = function(grunt) {
   grunt.initConfig({
 
+    //TODO: Output in multiple places (one for site, one for build)
     cssmin: {
-      files: {
+      build: {
         src: 'src/lib/css/*.css',
-        dest: 'dest/hagrid.css'
+        dest: 'build/hagrid.css'
+      },
+      site: {
+        src: 'src/lib/css/*.css',
+        dest: 'site/css/hagrid.css'
       }
     },
 
     less: {
-      files: {
+      build: {
         src: 'src/lib/less/hagrid.less',
-        dest: 'dest/hagrid.less.css'
+        dest: 'build/hagrid.less.css'
       }
+    },
+
+    // ASSEMBLE
+    assemble: {
+
+      // Global Options
+      options: {
+            layout: ['default.hbs'],
+            layoutdir: 'src/site/layouts',
+            partials: 'src/site/partials/*.hbs',
+            data: ['src/examples/data/*.{json,yml}'],
+            ext: '.html',
+            expand: true
+      },
+
+      site: {
+        files: [
+          // Index
+          {expand: true, cwd: 'src/examples/pages/', src: 'index.hbs', dest: 'site', ext: '.html'}
+        ]
+      }
+
     }
 
   });
   
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-handlebars');
+  grunt.loadNpmTasks('assemble');
 
   grunt.registerTask('default', ['cssmin', 'less']);
+  grunt.registerTask('compile', ['grunt-handlebars-compiler']);
 };
 
 
